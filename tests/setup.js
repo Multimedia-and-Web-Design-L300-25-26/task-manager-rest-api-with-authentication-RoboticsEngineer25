5999e28bc73cc49b9c2a0b101b68c105c5529de4
RoboticsEngineer25 <farrashosuman@gmail.com>
2026-03-05 14:17:59 +0000

    done with assignment

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,23 @@
-import app from "../src/app.js";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-export default app;
+let mongod;
+
+// Connect to in-memory MongoDB before all tests
+beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    process.env.MONGO_URI = uri;
+    process.env.JWT_SECRET = "test_secret_key_for_jest";
+    await mongoose.connect(uri);
+}, 30000);
+
+// Clear all collections after all tests in a suite complete
+afterAll(async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        await collections[key].deleteMany({});
+    }
+    await mongoose.connection.close();
+    await mongod.stop();
+}, 30000);
